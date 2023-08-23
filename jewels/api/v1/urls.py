8c46301b108d57f12ff67d0c403c4fd81640cc57
VtlBz <VtlBz@users.noreply.github.com>
2023-08-23 22:44:27 +0500

    Alfa v0.3.4

--- jewels/api/v1/urls.py
+++ jewels/api/v1/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -29,6 +30,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0),
          name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
--- jewels/api/v1/views.py
+++ jewels/api/v1/views.py
@@ -3,8 +3,6 @@ from django.contrib.postgres.aggregates import ArrayAgg
 from django.core.cache import cache
 from django.db import transaction
 from django.db.models import Sum
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
@@ -13,6 +11,9 @@ from rest_framework.response import Response
 from deals.models import Customer
 from utils import parse_deals, processing_top_qs
 from .serializers import FileUploadSerializer, TopSerializer
+
+
+cache_key_top = 'jewels-api-v1-deals-top-customers'
 
 
 class DealsViewSet(mixins.ListModelMixin,
@@ -61,7 +62,7 @@ class DealsViewSet(mixins.ListModelMixin,
                 with transaction.atomic():
                     Customer.objects.filter(is_actual=True).delete()
                     Customer.objects.all().update(is_actual=True)
-                cache.clear()
+                cache.delete(cache_key_top)
             except Exception as e:
                 Customer.objects.filter(is_actual=False).delete()
                 return Response(data={'Status': 'Error',
@@ -75,9 +76,8 @@ class DealsViewSet(mixins.ListModelMixin,
                               'Desc': serializer.errors},
                         status=status.HTTP_400_BAD_REQUEST)
 
-    @method_decorator(cache_page(None))
     @action(detail=False, methods=['get'])
-    def top(self, _):
+    def top(self, request):
         """
         Получение ТОП-5 покупателей.
 
@@ -86,11 +86,18 @@ class DealsViewSet(mixins.ListModelMixin,
         в список покупок у других покупателей.
         """
 
-        slice = settings.TOP_CUSTOMERS_COUNT
-        queryset = Customer.objects.values('username').annotate(
-            gems=ArrayAgg('deals__item', distinct=True),
-            spent_money=Sum('deals__total')
-        ).order_by('-spent_money')[:slice]
-        formatted_qs = processing_top_qs(queryset)
-        serializer = TopSerializer(formatted_qs)
-        return Response(serializer.data)
+        refresh = request.GET.get('refresh_cache') == 'true'
+
+        if not cache.get(cache_key_top) or refresh:
+
+            slice = settings.TOP_CUSTOMERS_COUNT
+            queryset = Customer.objects.values('username').annotate(
+                gems=ArrayAgg('deals__item', distinct=True),
+                spent_money=Sum('deals__total')
+            ).order_by('-spent_money')[:slice]
+            formatted_qs = processing_top_qs(queryset)
+            serializer = TopSerializer(formatted_qs)
+
+            cache.set(cache_key_top, serializer.data, timeout=None)
+
+        return Response(cache.get(cache_key_top))
